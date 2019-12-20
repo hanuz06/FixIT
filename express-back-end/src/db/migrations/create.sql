23a41e9d7a9e3649610deed13222a66fdbed3e10
Grant Taylor <granttaylor@Grants-MacBook-Pro-2.local>
2019-12-20 09:40:38 -0700

    changed create migrations to knex

--- express-back-end/src/db/migrations/create.sql
+++ express-back-end/src/db/migrations/create.sql
@@ -1,49 +1,86 @@
-DROP TABLE IF EXISTS rating CASCADE;
-DROP TABLE IF EXISTS inspection CASCADE;
-DROP TABLE IF EXISTS mechanic CASCADE;
-DROP TABLE IF EXISTS user CASCADE;
+// -- DROP TABLE IF EXISTS rating CASCADE;
+// -- DROP TABLE IF EXISTS inspection CASCADE;
+// -- DROP TABLE IF EXISTS mechanic CASCADE;
+// -- DROP TABLE IF EXISTS user CASCADE;
+knex.schema.dropTable('rating')
+knex.schema.dropTable('inspection')
+knex.schema.dropTable('mechanic')
+knex.schema.dropTable('user')
 
-CREATE TABLE user (
-  id SERIAL PRIMARY KEY NOT NULL,
-  first_name VARCHAR(255) NOT NULL,
-  last_name VARCHAR(255) NOT NULL,
-  email text not null unique,
-  password_digest text not null,
-  phone int,
-  location VARCHAR(255) NOT NULL
-);
+knex.schema.createTable('user', function (table) {
+  table.increments();
+  table.string('first_name');
+  table.string('last_name');
+  table.text('email');
+  table.text('password_digest');
+  table.integer('phone');
+  table.text('location')
 
-CREATE TABLE mechanic (
-  id SERIAL PRIMARY KEY NOT NULL,
-  first_name VARCHAR(255) NOT NULL,
-  last_name VARCHAR(255) NOT NULL,
-  email text not null unique,
-  password_digest text not null,
-  inspection_fee int,
-  phone int,
-  active boolean default FALSE,
-  description VARCHAR(255),
-  avatar VARCHAR(255) NOT NULL,
-  location VARCHAR(255) NOT NULL
-);
+})
 
-CREATE TABLE inspection (
-  id SERIAL PRIMARY KEY NOT NULL,
-  mechanic_id INTEGER REFERENCES mechanic(id) ON DELETE CASCADE,
-  user_id INTEGER REFERENCES user(id) ON DELETE CASCADE,
-  date VARCHAR(255) NOT NULL,
-  confirmed boolean default FALSE,
-  completed boolean default FALSE,
-  description VARCHAR(255),
-  car_make VARCHAR(255),
-  car_year VARCHAR(255),
+knex.schema.createTable('mechanic', function (table) {
+  table.increments();
+  table.string('first_name');
+  table.string('last_name');
+  table.text('email');
+  table.text('password_digest');
+  table.integer('phone');
+  table.text('location');
+  table.boolean('active');
+  table.text('description');
+  table.text("avatar");
+  table.text('location')
+})
 
-);
+knex.schema.createTable('inspection', function (table) {
 
-CREATE TABLE rating (
-  id SERIAL PRIMARY KEY NOT NULL,
-  mechanic_id INTEGER REFERENCES mechanic(id) ON DELETE CASCADE,
-  inspection_id INTEGER REFERENCES inspection(id) ON DELETE CASCADE,
-  user_id INTEGER REFERENCES user(id) ON DELETE CASCADE,
-  rating SMALLint
-);
+})
+
+knex.schema.createTable('rating', function (table) {
+
+})
+
+// -- CREATE TABLE mechanic (
+// --   id SERIAL PRIMARY KEY NOT NULL,
+// --   first_name VARCHAR(255) NOT NULL,
+// --   last_name VARCHAR(255) NOT NULL,
+// --   email text not null unique,
+// --   password_digest text not null,
+// --   inspection_fee int,
+// --   phone int,
+// --   active boolean default FALSE,
+// --   description VARCHAR(255),
+// --   avatar VARCHAR(255) NOT NULL,
+// --   location VARCHAR(255) NOT NULL
+// -- );
+
+// -- CREATE TABLE user (
+// --   id SERIAL PRIMARY KEY NOT NULL,
+// --   first_name VARCHAR(255) NOT NULL,
+// --   last_name VARCHAR(255) NOT NULL,
+// --   email text not null unique,
+// --   password_digest text not null,
+// --   phone int,
+// --   location VARCHAR(255) NOT NULL
+// -- );
+
+// -- CREATE TABLE inspection (
+// --   id SERIAL PRIMARY KEY NOT NULL,
+// --   mechanic_id INTEGER REFERENCES mechanic(id) ON DELETE CASCADE,
+// --   user_id INTEGER REFERENCES user(id) ON DELETE CASCADE,
+// --   date VARCHAR(255) NOT NULL,
+// --   confirmed boolean default FALSE,
+// --   completed boolean default FALSE,
+// --   description VARCHAR(255),
+// --   car_make VARCHAR(255),
+// --   car_year VARCHAR(255),
+
+// -- );
+
+// -- CREATE TABLE rating (
+// --   id SERIAL PRIMARY KEY NOT NULL,
+// --   mechanic_id INTEGER REFERENCES mechanic(id) ON DELETE CASCADE,
+// --   inspection_id INTEGER REFERENCES inspection(id) ON DELETE CASCADE,
+// --   user_id INTEGER REFERENCES user(id) ON DELETE CASCADE,
+// --   rating SMALLint
+// -- );
