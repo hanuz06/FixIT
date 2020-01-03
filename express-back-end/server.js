9f5c984668bd4849cddfad828046cbb883a62f78
granttaylor448 <granttaylor448@gmail.com>
2020-01-03 21:31:55 +0000

    add twilio message to mechanic

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -7,10 +7,17 @@ const knex = require('knex');
 // Express Configuration
 
 App.use(BodyParser.json());
-App.use(BodyParser.urlencoded({ extended: true }));
+App.use(BodyParser.urlencoded({ extended: false }));
 App.use(Express.static('public'));
 //DB
 const db = require("./src/db/db.js");
+
+// Twilio
+const client = require('twilio')(
+  process.env.TWILIO_ACCOUT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
 
 // Sample GET route
@@ -41,10 +48,30 @@ App.get('/api/inspections', async (req, res) => {
 });
 
 
-App.post('/api/new-inspections',(req, res) => {
+App.post('/api/new-inspections', (req, res) => {
+  res.header('Content-Type', 'application/json');
   console.log('requeeee ', req.body)  
   db('inspections').insert(req.body)
-  .then((res)=> console.log('result ', res) ) 
+  .returning('*') 
+  // START TWILIO MESSAGE
+  .then( async(res) => {  
+    // Helper function that finds the mechanics phone number
+    const mechanicNumber = await db('mechanics').where('id', res[0].mechanic_id).select('phone')
+    
+    client.messages
+      .create({
+        to: mechanicNumber[0].phone,
+        from: '+13064001290',
+        body: `Hello! We have a new service request for you. One of our clients who lives at 563 WoodPark Cres SW Calgary AB, has a service request for their ${res[0].car_make}. Here is their description of the problem: ${res[0].description_of_problem}. Please text back only "yes" if you would like to conifirm their appointment!`
+      })
+      .then((res) => {
+        console.log(res.body)
+      })
+      .catch(err => {
+        console.log(err);
+        // res.send(JSON.stringify({ success: false }));
+      });
+    }) 
   .catch((error)=> console.log('error ', error))   
 });
 
@@ -52,3 +79,19 @@ App.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Express seems to be listening on port ${PORT} so that's pretty good 👍`);
 });
+
+
+App.post('/sms-response', (req, res) => {
+  
+
+  const twiml = new MessagingResponse();
+
+  // Access the message body and the number it was sent from.
+  console.log(`Incoming message from ${req.body.From}: ${req.body.Body}`);
+
+  twiml.message('The Robots are coming! Head for the hills!');
+
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end(twiml.toString());
+    
+})
