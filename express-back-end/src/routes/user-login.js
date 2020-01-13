72d76d61df2366e9b01cec7a1560e5fdbf4b759b
Andrey Li <lee1975uz@gmail.com>
2020-01-13 11:53:26 -0700

    resolve conflicts in server.js

--- express-back-end/src/routes/user-login.js
+++ express-back-end/src/routes/user-login.js
@@ -1,29 +1,22 @@
 const express = require('express');
 const router  = express.Router();
 
-module.exports = (db) => {
+module.exports = (db, bcrypt) => {
   router.post('/', async(req, res) => {
-    console.log('LOGIN REQUEST RECEIVED BY PG: ', req.body);
     
     const {email, password} = req.body;
     
     const user = await db('users').where({email});
-    console.log('USER ', user);
+    
+     if (!user[0]) {
+       return res.status(400).json({ message: 'User not found' }) 
+      }
   
-    if (!user[0]) {
-      return res.status(400).json({ message: 'User not found' });
-    }
-   
-    let isMatch = false;
+     const isMatch = await bcrypt.compare(password, user[0].password_digest)
   
-    if (password === user[0].password_digest) {
-      isMatch = true;
-    }
-    if (isMatch === false) {
-      return res.status(404).json({ message: 'Password is incorrect' });
-    } else {
-      return res.status(200).json({ user });
-    }
+    if (!isMatch) {
+      return res.status(404).json({ message: 'Password is incorrect' })
+    } else { return res.status(200).json({ user }) }
   });
 
   return router;
--- express-back-end/src/routes/user-signup.js
+++ express-back-end/src/routes/user-signup.js
@@ -1,37 +1,46 @@
 const express = require('express');
 const router  = express.Router();
 
-module.exports = (db, check, validationResult) => {
+module.exports = (db, check, validationResult, bcrypt) => {
   router.post('/',[
-    check('password_digest', 'Password 6 characters required')
-      .isLength({ min: 6 })
-  ], async(req, res) => {
-    try {
-      
-      const errors = validationResult(req);
-  
-      if (!errors.isEmpty()) {
-        return res.status(422).json({
-          errors: errors.array(),
-          message: 'Incorrect input data'
-        });
-      }
-      
-      const findUser = await db('users').where({email: req.body.email});
+    check('password_digest', 'Password 3 characters required')
+    .isLength({ min: 3 })
+], async (req, res) => {
+
+  try {
     
-      console.log('findUser ', findUser.length);
-      if (findUser.length !== 0) {
-        console.log('EMAIL EXISTS');
-        return res.status(401).json({ message: 'Email exists' });
-      }
-  
-      //console.log('user not found...')
-      const userSignUpData = await db('users').insert(req.body, (['id', 'first_name', 'last_name']));
-     
-      res.status(200).json({userSignUpData, message: 'User successfully signed up' });
-    } catch (e) {
-      res.status(500).json({ message: 'Something is wrong, please try again' });
-    }
-  });
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({
+        errors: errors.array(),
+        message: 'Incorrect input data'
+      });
+    }    
+
+  const findUser = await db('users').where({email: req.body.email})  
+   
+   if (findUser.length !== 0) {     
+     return res.status(401).json({ message: 'User exists' })
+    } 
+
+  const hashedPassword = await bcrypt.hash(req.body.password_digest, 12)
+
+  const signUpData = {    
+    "first_name": req.body.first_name,
+    "last_name": req.body.last_name,
+    "email": req.body.email,
+    "password_digest": hashedPassword,
+    "phone": req.body.phone,
+    "location": req.body.location
+  }
+
+  const userSignUpData = await db('users').insert(signUpData, (['id', 'first_name', 'last_name']))  
+   
+    res.status(200).json({userSignUpData, message: 'User successfully signed up' });
+  } catch (e) {
+    res.status(500).json({ message: 'Something is wrong, please try again' });
+  }
+});
   return router;
 };
