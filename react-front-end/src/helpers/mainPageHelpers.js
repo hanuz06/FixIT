0d6ccd663512c3580077dd4d01719ea7425f015e
Andrey Li <lee1975uz@gmail.com>
2020-04-01 14:30:00 -0600

    add function name in mainPageHelpers

--- react-front-end/src/helpers/mainPageHelpers.js
+++ react-front-end/src/helpers/mainPageHelpers.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import useScrollTrigger from '@material-ui/core/useScrollTrigger';
-import Zoom from '@material-ui/core/Zoom';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import Zoom from "@material-ui/core/Zoom";
+import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles(theme  => ({  
+const useStyles = makeStyles(theme => ({
   backToTopButton: {
-    position: 'fixed',
+    position: "fixed",
     bottom: theme.spacing(2),
     right: theme.spacing(2),
-    opacity: '0.7'
+    opacity: "0.7"
   }
 }));
 
-export function (props) {
+export function ScrollTop(props) {
   const { children, window } = props;
   const classes = useStyles();
 
@@ -21,20 +21,26 @@ export function (props) {
   const trigger = useScrollTrigger({
     target: window ? window() : undefined,
     disableHysteresis: true,
-    threshold: 100,
+    threshold: 100
   });
 
   const handleClick = event => {
-    const anchor = (event.target.ownerDocument || document).querySelector('#back-to-top');
+    const anchor = (event.target.ownerDocument || document).querySelector(
+      "#back-to-top"
+    );
 
     if (anchor) {
-      anchor.scrollIntoView({behavior:'smooth'});
+      anchor.scrollIntoView({ behavior: "smooth" });
     }
   };
 
   return (
     <Zoom in={trigger}>
-      <div onClick={handleClick} role="presentation" className={classes.backToTopButton}>
+      <div
+        onClick={handleClick}
+        role="presentation"
+        className={classes.backToTopButton}
+      >
         {children}
       </div>
     </Zoom>
